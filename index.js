44a7b43fe82ab7cfc89d2a80329c9a26a759a888
Shahil-Dev <alfee9006@gmail.com>
2024-10-21 11:33:04 +0600

    fourth commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// script.js
 document.querySelector('.menu-toggle').addEventListener('click', () => {
     const navItems = document.querySelector('.nav-items');
     navItems.classList.toggle('nav-active');
